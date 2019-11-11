100c7bfbe4099af451f63755a5243cb9263c80db
tim310579 <43834434+tim310579@users.noreply.github.com>
2019-11-11 12:06:10 +0800

    要發瘋啦

--- lab5-3/src/main.c
+++ lab5-3/src/main.c
@@ -12,7 +12,7 @@
 #define Y2 GPIO_Pin_4
 #define Y3 GPIO_Pin_3
 
-//unsigned int x_pin = {X0, X1, X2, X3};
+//unsigned int x_pin[4] = {X0, X1, X2, X3};
 //unsigned int y_pin = {Y0, Y1, Y2, Y3};
 
 // initial keypad gpio pin, X as output and Y as input
@@ -20,14 +20,14 @@
 void keypad_init()
 {
 
-	RCC->AHB2ENR = RCC->AHB2ENR|0x2;
+	RCC->AHB2ENR = RCC->AHB2ENR|0x6;
 	GPIOC->MODER   &= 0b11111111111111111111111100000000;
 	GPIOC->MODER   |= 0b00000000000000000000000001010101;
 	GPIOC->PUPDR   &= 0b11111111111111111111111100000000;
 	GPIOC->PUPDR   |= 0b00000000000000000000000001010101;
 	GPIOC->OSPEEDR &= 0b11111111111111111111111100000000;
 	GPIOC->OSPEEDR |= 0b00000000000000000000000001010101;
-	GPIOC->ODR     |= 0b0000000000001111;
+	GPIOC->ODR     |= 0b00000000000000000000000000001111;
 
 	GPIOB->MODER   &= 0b11111111111111111100000000111111;
 	GPIOB->PUPDR   &= 0b11111111111111111100000000111111;
@@ -105,6 +105,7 @@ signed char keypad_scan()
 		return -1;
 }
 signed char keypad_scan_back(){
+
 	//GPIOC->BRR = X0;
 	//GPIOC->BRR = X1;
 	//GPIOC->BRR = X2;
@@ -185,12 +186,14 @@ int main()
 
 			input = keypad_scan();
 			tmp_input = keypad_scan_back();
-			if(input == tmp_input)	sum = -1;
+			sum = -1;
 			if(input == tmp_input && input != -1)	{
 				//choose[input] = 1;
 				//cnt = 1;
 				sum = input;
+
 			}
+
 			if(input != tmp_input && input != -1 && tmp_input != -1) {
 				//choose[input] = 1;
 				//choose[tmp_input] = 1;
@@ -200,7 +203,7 @@ int main()
 
 
 			int dig = 0;
-			if(sum > 9) dig = 2;
+			if(sum > 9 && sum < 100) dig = 2;
 			else if(sum >= 0) dig = 1;
 
 			if(dig > 0)	display(sum, dig);
