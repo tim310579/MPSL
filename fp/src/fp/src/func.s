3fa7028df38afb5b0bd6dc1c5e7e151a27e650df
tim310579 <43834434+tim310579@users.noreply.github.com>
2019-12-14 23:46:04 +0800

    success

---
+++ fp/src/fp/src/func.s
@@ -0,0 +1,146 @@
+.syntax unified
+	.cpu cortex-m4
+	.thumb
+
+.text
+
+	.global GPIO_init
+	.global max7219_init
+	.global max7219_send
+	.global Delay1sWith4MHz
+	.global fpu_enable
+
+	.equ RCC_AHB2ENR, 0x4002104C
+
+	.equ DECODE_MODE, 0x09
+	.equ INTENSITY, 0x0A
+	.equ SCAN_LIMIT, 0x0B
+	.equ SHUT_DOWN, 0x0C
+	.equ DISPLAY_TEST, 0x0F
+
+	.equ DATA, 0x20 //PA5
+	.equ LOAD, 0x40 //PA6
+	.equ CLOCK, 0x80 //PA7
+
+	.equ GPIOA_MODER, 0x48000000
+	.equ GPIOA_OTYPER, 0x48000004
+	.equ GPIOA_SPEEDER, 0x48000008
+
+	.equ GPIOB_MODER, 0x48000400
+	.equ GPIOC_MODER, 0x48000800
+	.equ GPIOA_BSRR, 0x48000018 // set bit
+	.equ GPIOA_BRR, 0x48000028 // clear bit
+
+	.equ GPIOC_IDR, 0x48000810	//user button
+
+GPIO_init:
+	push {r0, r1, r2, lr}
+
+	movs r0, #5
+	ldr r1, =RCC_AHB2ENR
+	str r0, [r1]
+
+	ldr r1, =GPIOA_MODER
+	ldr r2, [r1]
+	and r2, 0xFFFF03FF	//set PA5,6,7
+	orr r2, 0x5400
+	str r2, [r1]
+
+	ldr r1, =GPIOA_OTYPER
+	ldr r2, [r1]
+	and r2, 0xFFFFFF1F
+	str r2, [r1]
+
+	ldr r1, =GPIOA_SPEEDER
+	ldr r2, [r1]
+	and r2, 0xFFFF03FF
+	orr r2, 0x5400
+	str r2, [r1]
+
+	ldr  r1, =GPIOB_MODER @ GPIOB_MODER
+	ldr  r2, [r1]
+	and  r2, 0b11111111111111111111111100111111
+	orr  r2, 0b00000000000000000000000010000000 @ alternate function mode for PB3
+	str  r2, [r1]
+
+	ldr r1, =GPIOC_MODER
+	ldr r2, [r1]
+	and r2, 0xF3FFFFFF
+	str r2, [r1]
+
+	pop  {r0, r1, r2, pc}
+max7219_init:
+	push {r0, r1, lr}
+
+	ldr r0, =#DECODE_MODE
+	ldr r1, =#0xFF
+	bl max7219_send
+
+	ldr r0, =#DISPLAY_TEST
+	ldr r1, =#0x0
+	bl max7219_send
+
+	ldr r0, =#SCAN_LIMIT
+	ldr r1, =0x7
+	bl max7219_send
+
+	ldr r0, =#INTENSITY
+	ldr r1, =#0xA
+	bl max7219_send
+
+	ldr r0, =#SHUT_DOWN
+	ldr r1, =#0x1
+	bl max7219_send
+
+	pop {r0, r1, pc}
+max7219_send:
+	push {r0, r1, r2, r3, r4, r5, r6, r7, r8, lr}	//r0->address
+	lsl r0, 0x8
+	add r0, r1
+	ldr r1, =GPIOA_MODER
+	ldr r2, =LOAD
+	ldr r3, =DATA
+	ldr r4, =CLOCK
+	ldr r5, =GPIOA_BSRR
+	ldr r6, =GPIOA_BRR
+	ldr r7, =0x0F
+
+max7219_send_loop:
+	mov r8, 0x1
+	lsl r8, r8, r7
+	str r4, [r6] //clk -> 0
+	tst r0, r8 // ands but discard result
+	beq max7219_send_clear
+	str r3, [r5] // din -> 1
+	b max7219_send_check
+
+max7219_send_clear:
+	str r3, [r6] // din -> 0
+
+max7219_send_check:
+	str r4, [r5] // clk -> 1
+	subs r7, 0x1
+	bge max7219_send_loop
+	str r2, [r6] // cs -> 0
+	str r2, [r5] // cs -> 1
+	pop {r0, r1, r2, r3, r4, r5, r6, r7, r8, pc}
+Delay1sWith4MHz:
+	push {r0, lr}
+	ldr r0, =1000000
+	movs r0, r0
+Delay1sWith4MHz_loop:
+	beq delay_end
+	subs r0, r0, 1
+	b Delay1sWith4MHz_loop
+delay_end:
+	pop {r0, pc}
+
+fpu_enable:
+	push  {r0, r1, lr}
+	ldr.w r0, =0xE000ED88
+	ldr   r1, [r0]
+	orr   r1, r1, #(0xF << 20)
+	str   r1, [r0]
+	dsb
+	isb
+	pop   {r0, r1, pc}
---
+++ fp/src/fp/src/main.c
@@ -0,0 +1,75 @@
+#include "stm32l476xx.h"
+#include "utils.h"
+
+#include "core_cm4.h"
+
+
+void keypad_init()
+{
+	RCC->AHB2ENR   |= 0b00000000000000000000000000000110;
+	GPIOB->MODER   &= 0b11111111111111110000000011111111;
+	GPIOB->PUPDR   &= 0b11111111111111110000000011111111;
+	GPIOB->PUPDR   |= 0b00000000000000001010101000000000;
+	GPIOB->OSPEEDR &= 0b11111111111111110000000011111111;
+	GPIOB->OSPEEDR |= 0b00000000000000000101010100000000;
+}
+
+void timer_init()
+{
+	RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN;
+		// enable TIM2 timer clock
+		GPIOB->AFR[0] |= GPIO_AFRL_AFSEL3_0;
+		// select AF1 for PB3 (PB3 is TIM2_CH2)
+		TIM2->CR1 |= TIM_CR1_DIR;
+		// counter used as downcounter
+		//TIM2->CR1 |= TIM_CR1_ARPE;
+		// enable auto-reload preload (buffer TIM2_ARR)
+		TIM2->ARR = (uint32_t) 100;
+		// auto-reload prescaler value
+		TIM2->CCMR1 &= 0xFFFFFCFF;
+		// select compare 2 (channel 2 is configured as output)
+		TIM2->CCMR1 |= (TIM_CCMR1_OC2M_2 | TIM_CCMR1_OC2M_1);
+		// set output compare 2 mode to PWM mode 1
+		TIM2->CCMR1 |= TIM_CCMR1_OC2PE;
+		// enable output compare 2 preload register on TIM2_CCR2
+		TIM2->CCER |= TIM_CCER_CC2E;
+		// enable compare 2 output
+		TIM2->EGR = TIM_EGR_UG;
+		// re-initialize the counter and generates an update of the registers
+
+}
+
+void timer_config()
+{
+
+
+	TIM2->PSC = (uint32_t) 800;
+	// prescaler value
+	TIM2->CCR2 = 10;
+	// compare 2 preload value
+}
+
+int main()
+{
+
+	fpu_enable();
+
+	GPIO_init();
+	keypad_init();
+	timer_init();
+	int i = 0;
+	float change[6] = {2.5, 5, 7.5, 10, 12.5, 15};
+	for(i = 0; i < 6; i++){
+		TIM2->PSC = (uint32_t) (4000000/50/100);	//period = 0.02sec
+		//¿é¥X¯ßªi¶g´Á = (TIM_Period + 1) * (TIM_Prescaler + 1) / TIMxCLK
+		TIM2->CCR2 = change[i];		//change this vlaue to 2.5, 5, 7.5, 10, 12.5, 15
+		TIM2->CR1 |= TIM_CR1_CEN;
+		int j = 400000;
+		while(j > 0){
+			j--;
+		}
+	}
+
+}
+
+
