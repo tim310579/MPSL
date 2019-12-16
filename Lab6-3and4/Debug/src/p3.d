40c7681c4945c7efccd2853abc3a58c345f44f86
william0206 <57868622+william0206@users.noreply.github.com>
2019-12-16 16:17:56 +0800

    tim2_pb3_success

---
+++ Lab6-3and4/Debug/src/p3.d
@@ -0,0 +1,25 @@
+src/p3.o: ../src/p3.c \
+ /Users/williamchen/Documents/workspace/Lab6-3and4/inc/stm32l476xx.h \
+ /Users/williamchen/Documents/workspace/Lab6-3and4/inc/core_cm4.h \
+ /Users/williamchen/Documents/workspace/Lab6-3and4/inc/core_cmInstr.h \
+ /Users/williamchen/Documents/workspace/Lab6-3and4/inc/cmsis_gcc.h \
+ /Users/williamchen/Documents/workspace/Lab6-3and4/inc/core_cmFunc.h \
+ /Users/williamchen/Documents/workspace/Lab6-3and4/inc/core_cmSimd.h \
+ /Users/williamchen/Documents/workspace/Lab6-3and4/inc/system_stm32l4xx.h \
+ /Users/williamchen/Documents/workspace/Lab6-3and4/inc/utils.h
+
+/Users/williamchen/Documents/workspace/Lab6-3and4/inc/stm32l476xx.h:
+
+/Users/williamchen/Documents/workspace/Lab6-3and4/inc/core_cm4.h:
+
+/Users/williamchen/Documents/workspace/Lab6-3and4/inc/core_cmInstr.h:
+
+/Users/williamchen/Documents/workspace/Lab6-3and4/inc/cmsis_gcc.h:
+
+/Users/williamchen/Documents/workspace/Lab6-3and4/inc/core_cmFunc.h:
+
+/Users/williamchen/Documents/workspace/Lab6-3and4/inc/core_cmSimd.h:
+
+/Users/williamchen/Documents/workspace/Lab6-3and4/inc/system_stm32l4xx.h:
+
+/Users/williamchen/Documents/workspace/Lab6-3and4/inc/utils.h:
